05d0eff839af277e7049b17fb3e8cac173b9dfd6
Yeeax <36502994+Yeeax@users.noreply.github.com>
2018-08-01 13:11:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
 client.on('ready', () => {
     console.log(`Logeada!`);
-    client.user.setGame("09.07.18 🌹");
+    client.user.setGame("Mutluluk hayaller içindir.");
     
   
   });
